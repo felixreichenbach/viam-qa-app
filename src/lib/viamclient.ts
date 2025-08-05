55eeff860b2a6dc5449dbacd20f3208a411a457d
felixreichenbach <felix.reichenbach@viam.com>
2025-08-05 21:30:42 +0200

    store inference results as tabular data

--- src/lib/viamclient.ts
+++ src/lib/viamclient.ts
@@ -1,3 +1,4 @@
+import type { Class } from '@tensorflow-models/tasks/dist/tasks/common';
 import * as VIAM from '@viamrobotics/sdk';
 
 const API_KEY_ID = import.meta.env.VITE_API_KEY_ID;
@@ -17,13 +18,31 @@ export async function connect(): Promise<VIAM.ViamClient> {
 	return await VIAM.createViamClient(opts);
 }
 
-export async function uploadData(binaryData: Uint8Array, tags: string[]): Promise<string> {
+export async function uploadData(
+	binaryData: Uint8Array,
+	classifications: Class[],
+	user_classification: string
+): Promise<string> {
+	// Get the label with the highest score, excluding 'VIAM_UNKNOWN'
+	let tags = [];
+	const filtered = classifications.filter((c) => c.className !== 'VIAM_UNKNOWN');
+	if (filtered.length > 0) {
+		const top = filtered.reduce((a, b) => (a.score > b.score ? a : b));
+		tags.push('MODEL_' + top.className);
+	}
+	// Add user classification and other tags
+	tags.push(user_classification);
+	tags.push('qa-app');
+
 	if (!viamClient) {
 		// If the client is not initialized, create a new one
 		viamClient = await connect();
 	}
 
-	tags.push('qa-app');
+	console.log('Uploading data with tags:', tags);
+	console.log(classifications);
+	console.log(user_classification);
+
 	const id = await viamClient.dataClient.binaryDataCaptureUpload(
 		binaryData,
 		PART_ID,
@@ -35,6 +54,25 @@ export async function uploadData(binaryData: Uint8Array, tags: string[]): Promis
 		tags
 	);
 	await viamClient.dataClient.addTagsToBinaryDataByIds(tags, [id]);
+
+	const inferenceResult: Record<string, VIAM.JsonValue> = {
+		classifications: classifications.map((c) => ({
+			className: c.className,
+			score: c.score
+		})),
+		user_classification: user_classification,
+		image_id: id
+	};
+
+	await viamClient.dataClient.tabularDataCaptureUpload(
+		[{ readings: inferenceResult }],
+		PART_ID,
+		'rdk:component:sensor',
+		'qa-app',
+		'Readings',
+		[[new Date(), new Date()]]
+	);
+
 	return id;
 }
 
